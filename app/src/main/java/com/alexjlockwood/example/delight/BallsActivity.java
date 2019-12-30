a953a2f7f6ccc8554695558a6111c51a9ac1f2fc
yagray <yagray@mail.ru>
2019-12-30 17:53:41 +0300

    3 шарика прыгают по очереди

--- app/src/main/java/com/alexjlockwood/example/delight/BallsActivity.java
+++ app/src/main/java/com/alexjlockwood/example/delight/BallsActivity.java
@@ -1,7 +1,10 @@
 package com.alexjlockwood.example.delight;
 
 import android.graphics.drawable.Animatable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.graphics.drawable.Animatable2Compat;
+import android.support.graphics.drawable.AnimatedVectorDrawableCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
 
@@ -11,17 +14,33 @@ import butterknife.OnClick;
 
 public class BallsActivity extends AppCompatActivity {
 
-    @BindView(R.id.balls) ImageView ballsView;
+//    @BindView(R.id.balls) ImageView ballsView;
+    @BindView(R.id.iv_jumping_balls) ImageView jumpingBallsView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_balls);
         ButterKnife.bind(this);
+
+        AnimatedVectorDrawableCompat avd =
+        AnimatedVectorDrawableCompat.create(this, R.drawable.avd_jumping_balls);
+
+        jumpingBallsView.setImageDrawable(avd);
+
+        if(avd != null) {
+            avd.registerAnimationCallback(new Animatable2Compat.AnimationCallback() {
+                @Override
+                public void onAnimationEnd(Drawable drawable) {
+                    jumpingBallsView.post(avd::start);
+                }
+            });
+            avd.start();
+        }
     }
 
-    @OnClick(R.id.balls)
-    void onClick() {
-        ((Animatable) ballsView.getDrawable()).start();
-    }
+//    @OnClick(R.id.balls)
+//    void onClick() {
+//        ((Animatable) ballsView.getDrawable()).start();
+//    }
 }
