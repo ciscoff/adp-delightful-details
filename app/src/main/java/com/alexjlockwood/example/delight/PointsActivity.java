0a8f76000ab6ac4f1bf9c509ad40b9d616e606ab
yagray <yagray@mail.ru>
2020-01-10 10:06:48 +0300

    Точки мигают. Но нужно что-то покруче

--- app/src/main/java/com/alexjlockwood/example/delight/PointsActivity.java
+++ app/src/main/java/com/alexjlockwood/example/delight/PointsActivity.java
@@ -1,10 +1,7 @@
 package com.alexjlockwood.example.delight;
 
-
-import android.animation.ObjectAnimator;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.widget.ImageView;
 
 import java.util.ArrayList;
@@ -21,16 +18,6 @@ import io.reactivex.schedulers.Schedulers;
 
 public class PointsActivity extends AppCompatActivity {
 
-//    private static final int[] STATE_SET_P1 =
-//            {R.attr.state_point1, -R.attr.state_point2, -R.attr.state_point3, -R.attr.state_point4};
-//    private static final int[] STATE_SET_P2 =
-//            {-R.attr.state_point1, R.attr.state_point2, -R.attr.state_point3, -R.attr.state_point4};
-//    private static final int[] STATE_SET_P3 =
-//            {-R.attr.state_point1, -R.attr.state_point2, R.attr.state_point3, -R.attr.state_point4};
-//    private static final int[] STATE_SET_P4 =
-//            {-R.attr.state_point1, -R.attr.state_point2, -R.attr.state_point3, R.attr.state_point4};
-
-
     private static final int[] STATE_SET_P1 =
             {R.attr.state_point1};
     private static final int[] STATE_SET_P2 =
@@ -41,7 +28,7 @@ public class PointsActivity extends AppCompatActivity {
             {R.attr.state_point4};
 
     private static ArrayList<int[]> states = new ArrayList<>(
-            Arrays.asList(STATE_SET_P1, STATE_SET_P2, STATE_SET_P3, STATE_SET_P4)
+            Arrays.asList(STATE_SET_P4, STATE_SET_P1, STATE_SET_P2, STATE_SET_P3)
     );
 
     Disposable disposable = null;
@@ -56,7 +43,7 @@ public class PointsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_points);
         ButterKnife.bind(this);
 
-        ticks = createTickObservable(1000);
+        ticks = createTickObservable(400);
     }
 
     @Override
@@ -67,16 +54,10 @@ public class PointsActivity extends AppCompatActivity {
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(
                         l -> {
-                            System.out.println("APP_TAG: next state " + states.get(0)[0]);
                             blinkingPoints.setImageState(states.get(0), false);
                             Collections.rotate(states, states.size() - 1);
-                            Log.d("APP_TAG", "tick " + l);
-                            System.out.println("APP_TAG: tick " + l);
                         },
-                        e -> {
-                            System.out.println("APP_TAG: " + e.getMessage());
-                            Log.d("APP_TAG", e.getStackTrace().toString());
-                        }
+                        e -> System.out.println("APP_TAG: " + e.getMessage())
                 );
     }
 
@@ -89,9 +70,7 @@ public class PointsActivity extends AppCompatActivity {
     }
 
     private Observable<Long> createTickObservable(long updateInterval) {
-
         return Observable
-                .interval(0, updateInterval, TimeUnit.MILLISECONDS)
-                .subscribeOn(Schedulers.computation());
+                .interval(0, updateInterval, TimeUnit.MILLISECONDS, Schedulers.io());
     }
 }
