1da3afff7374ced5b316d2668ce098370653b50f
yagray <yagray@mail.ru>
2020-01-10 11:21:03 +0300

    Супер ! Анимашка с двигающимися точками !

--- app/src/main/java/com/alexjlockwood/example/delight/MainActivity.java
+++ app/src/main/java/com/alexjlockwood/example/delight/MainActivity.java
@@ -17,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
         LIST_ITEM_TO_ACTIVITY_MAP.put(R.id.design, DesignActivity.class);
         LIST_ITEM_TO_ACTIVITY_MAP.put(R.id.spinner, SpinnerActivity.class);
         LIST_ITEM_TO_ACTIVITY_MAP.put(R.id.points, PointsActivity.class);
+        LIST_ITEM_TO_ACTIVITY_MAP.put(R.id.movingPoints, MovingPointsActivity.class);
         LIST_ITEM_TO_ACTIVITY_MAP.put(R.id.writer, WriterActivity.class);
         LIST_ITEM_TO_ACTIVITY_MAP.put(R.id.globus, GlobusActivity.class);
         LIST_ITEM_TO_ACTIVITY_MAP.put(R.id.my, MyHandwritingActivity.class);
@@ -48,6 +49,7 @@ public class MainActivity extends AppCompatActivity {
             R.id.design,
             R.id.spinner,
             R.id.points,
+            R.id.movingPoints,
             R.id.my,
             R.id.writer,
             R.id.globus,
